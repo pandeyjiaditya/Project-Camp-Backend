a38522e82bdc6a5533c6f9d44346c5d0ada9bf6c
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-20 19:24:47 +0530

    .env setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env"
+})
+
+let myUsername = process.env.name
+console.log("value: ", myUsername);
+
 console.log("Start of an backend project");
