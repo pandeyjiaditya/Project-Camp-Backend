32cfc90c8f669ca66371fdbf55fd43d00586ec0e
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-22 08:22:54 +0530

    basic express configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import express from "express"
 
 const app = express()
 
+//basic configuration
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
   res.send("welcome to ProjectCampy !!");
 });
