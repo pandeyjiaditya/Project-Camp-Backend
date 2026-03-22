56c662f72e48d6ab4fc76dea85180f03ce8f0430
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-22 08:29:48 +0530

    cors configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from "express"
-
+import cors from "cors"
 const app = express()
 
 //basic configuration
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+
+//cors configuration
+app.use(
+  cors({
+          origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+          credentials: true,
+          mathods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+          allowedHeaders: ["Authorization", "Content-Type"]
+       })
+);
 
 app.get("/", (req, res) => {
   res.send("welcome to ProjectCampy !!");
