c956cfe6feb4c7eab8dbd148ff378e43f8a8c1c1
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-22 08:34:28 +0530

    apiResponse

---
+++ src/utils/api-response.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode, Data, message = "Success"){
+        this.statusCode = statusCode
+        this.Data = Data
+        this.message = message
+        this.success = statusCode < 400 
+    }
+}
