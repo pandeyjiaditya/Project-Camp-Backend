3321c027197df5911620d500473d19eeb242a518
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-04-02 15:00:31 +0530

    error check

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,8 +2,10 @@ import { ApiResponse } from "../utils/api-response.js";
 
 const healthCheck = (req, res) => {
     try {
-        res.status(200).json(
-            new ApiResponse(200, {message : "Server is Running"})
+        const user = getUserFromDB()
+        res
+            .status(200)
+            .json(new ApiResponse(200, {message : "Server is Running"})
         )
     } catch (error) {}
 };
